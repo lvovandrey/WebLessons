35e9e06bff2ea189506c1539e6b9232d538e245f
lvovandrey <lvovandrey@mail.ru>
2021-04-12 16:59:07 +0300

    add fonts

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,18 @@
-var gulp = require('gulp');
-var sass = re
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+
+// Static server
+gulp.task('server', function() {
+    browserSync.init({
+        server: {
+            port: 9000,
+            baseDir: "build"
+        }
+    });
+
+    gulp.watch('build/**/*').on('change', browserSync.reload);
+});
+
 gulp.task('mytask', async function() {
     console.log('Hi from gulp-task');
 });
